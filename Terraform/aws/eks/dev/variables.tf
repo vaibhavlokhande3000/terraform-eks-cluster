3213f1c47231410011a77a937d723d01dbe15a4d
vaibhavlokhande3000 <65537847+vaibhavlokhande3000@users.noreply.github.com>
2026-01-27 17:02:00 +0530

    Update variables.tf

--- Terraform/aws/eks/dev/variables.tf
+++ Terraform/aws/eks/dev/variables.tf
@@ -1,9 +1,10 @@
 variable "region" {
   type    = string
-  default = "us-east-1"
+  default = "us-east-2"
 }
 
 variable "cluster_name" {
   type        = string
   description = "Must match the cluster_name used in the VPC stack"
+
 }
