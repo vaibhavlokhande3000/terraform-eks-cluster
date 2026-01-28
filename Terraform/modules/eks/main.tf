ceca20ddfe3118b70dccaf57cd02e6b8bff86efb
vaibhavlokhande3000 <65537847+vaibhavlokhande3000@users.noreply.github.com>
2026-01-28 14:05:01 +0530

    Update main.tf

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -1,39 +1,10 @@
-resource "aws_eks_cluster" "this" {
-  name     = var.cluster_name
-  role_arn = var.cluster_role_arn
-
-vpc_config {
-
-    subnet_ids = concat(var.public_subnet_ids, var.private_subnet_ids)
-    endpoint_private_access = true
-    endpoint_public_access  = true
-  }
-}
-
-resource "aws_eks_node_group" "this" {
-  cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "private-node-group"
-  node_role_arn   = var.node_role_arn
-
-  subnet_ids      = var.private_subnet_ids
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
-
-instance_types = ["t2.medium"]
-
-###Adding sg
-
-# Security Group
-resources "aws_security_group" "app_sg" {
+# 1. The Security Group (Defined First)
+resource "aws_security_group" "app_sg" {
   name        = "${var.cluster_name}-app-sg"
   description = "Allow ports 5000 and 8080"
   vpc_id      = var.vpc_id
 
-  ingress { 
+  ingress {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
@@ -59,24 +30,38 @@ resources "aws_security_group" "app_sg" {
   }
 }
 
-# Attach it to the Cluster
-resources "aws_eks_cluster" "this" {
+# 2. The EKS Cluster (Attaching the Security Group)
+resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn
 
   vpc_config {
-    # Add the new Security Group here
-    security_group_ids      = [aws_security_group.app_sg.id]
-    
+    # Attach the Security Group ID here
+    security_group_ids = [aws_security_group.app_sg.id]
+
     subnet_ids              = concat(var.public_subnet_ids, var.private_subnet_ids)
     endpoint_private_access = true
     endpoint_public_access  = true
   }
 }
-depends_on = [
+
+# 3. The Node Group (Depends on Cluster)
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "private-node-group"
+  node_role_arn   = var.node_role_arn
+
+  subnet_ids = var.private_subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t2.medium"]
+
+  depends_on = [
     aws_eks_cluster.this
   ]
-
 }
-
-
