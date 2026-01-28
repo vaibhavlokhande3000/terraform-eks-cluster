53083b511558559c42b4400eada2ab5ab9d21a82
vaibhavlokhande3000 <65537847+vaibhavlokhande3000@users.noreply.github.com>
2026-01-28 12:34:05 +0530

    Update variables.tf

--- Terraform/modules/eks/variables.tf
+++ Terraform/modules/eks/variables.tf
@@ -4,3 +4,4 @@ variable "node_role_arn" {}
 variable "vpc_cidr" {}
 variable "public_subnet_ids" { type = list(string) }
 variable "private_subnet_ids" { type = list(string) }
+variable "vpc_id" { type = string}
