5b12e82b640d559c646be97ca8398d40996b82ea
vaibhavlokhande3000 <65537847+vaibhavlokhande3000@users.noreply.github.com>
2026-01-28 17:48:23 +0530

    Update backend.tf

--- Terraform/aws/eks/dev/backend.tf
+++ Terraform/aws/eks/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-ironmanav2" 
-    key            = "path/to/myproject.tfstate"             
+    bucket         = "tfstate-ironmanav" 
+    key            = "eks/terraform.tfstate"             
     region         = "us-east-2"        
     dynamodb_table = "terraform-state-lock"                
     encrypt        = true                                    
