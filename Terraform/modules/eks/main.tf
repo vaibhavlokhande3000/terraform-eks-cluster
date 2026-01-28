62c74ed54c9441c40726268499a8132b2464d0f3
vaibhavlokhande3000 <65537847+vaibhavlokhande3000@users.noreply.github.com>
2026-01-28 12:33:23 +0530

    Update main.tf

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -25,7 +25,56 @@ resource "aws_eks_node_group" "this" {
 
 instance_types = ["t2.medium"]
 
+###Adding sg
+
+# Security Group
+resource "aws_security_group" "app_sg" {
+  name        = "${var.cluster_name}-app-sg"
+  description = "Allow ports 5000 and 8080"
+  vpc_id      = var.vpc_id
+
+  ingress { 
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-app-sg"
+  }
+}
+
+# Attach it to the Cluster
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    # Add the new Security Group here
+    security_group_ids      = [aws_security_group.app_sg.id]
+    
+    subnet_ids              = concat(var.public_subnet_ids, var.private_subnet_ids)
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+}
 depends_on = [
     aws_eks_cluster.this
   ]
+
 }
