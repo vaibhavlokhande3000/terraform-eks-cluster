4cbad043138a4376b1f386fb46f3c29a92bfa842
vaibhavlokhande3000 <65537847+vaibhavlokhande3000@users.noreply.github.com>
2026-01-29 13:02:38 +0530

    Update subnets.tf

--- Terraform/modules/vpc/subnets.tf
+++ Terraform/modules/vpc/subnets.tf
@@ -17,6 +17,7 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidr)
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.private_subnet_cidr[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
@@ -27,3 +28,4 @@ resource "aws_subnet" "private" {
   }
 
 }
+
