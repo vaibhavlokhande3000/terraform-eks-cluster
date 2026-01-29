594af49c666200ace45ebcd30b758c44a3be1664
vaibhavlokhande3000 <vlokhande3000@gmail.com>
2026-01-29 11:16:09 +0000

    autoscalar and metrics server added

--- Terraform/aws/eks/dev/main.tf
+++ Terraform/aws/eks/dev/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+
 provider "aws" {
   region = var.region
 }
