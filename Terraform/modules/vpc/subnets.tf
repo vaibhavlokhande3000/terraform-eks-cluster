61ec6b4cc83eefcf86554cb79638bea287b5187a
vaibhavlokhande3000 <65537847+vaibhavlokhande3000@users.noreply.github.com>
2026-01-29 12:48:58 +0530

    Update subnets.tf

--- Terraform/modules/vpc/subnets.tf
+++ Terraform/modules/vpc/subnets.tf
@@ -18,11 +18,12 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.this.id
-  cidr_block        = var.private_subnet_cidr
-  availability_zone = data.aws_availability_zones.available.names[1]
+  cidr_block        = var.private_subnet_cidr[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = {
-    Name = "${var.cluster_name}-private"
+    Name = "${var.cluster_name}-private-${count.index + 1}"
     "kubernetes.io/role/internal-elb" = "1"
   }
+
 }
