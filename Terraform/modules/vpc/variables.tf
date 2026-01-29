77418b39a68e86e50f8fff87c052eb7a28840160
vaibhavlokhande3000 <65537847+vaibhavlokhande3000@users.noreply.github.com>
2026-01-29 12:46:59 +0530

    Update variables.tf

--- Terraform/modules/vpc/variables.tf
+++ Terraform/modules/vpc/variables.tf
@@ -1,4 +1,5 @@
 variable "vpc_cidr" {}
 variable "cluster_name" {}
 variable "public_subnet_cidr" {type = string}
-variable "private_subnet_cidr" {type = string}
+
+variable "private_subnet_cidr" {type = list(string)}
